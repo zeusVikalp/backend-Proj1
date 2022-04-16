9a99e341ee51c7749123706c53b88f5e85f6d026
zeusVikalp <100758326+zeusVikalp@users.noreply.github.com>
2022-04-16 15:30:53 +0530

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = require('./add.js')
+const app1 = require('./mul.js')
+const app2 = require('./div.js')
+const app3 = require('./sub.js')
+
+console.log(app.sum(10,20))
+console.log(app1.mul(10,20))
+console.log(app2.div(10,20))
+console.log(app3.sub(10,20))
---
+++ sub.js
@@ -0,0 +1,6 @@
+function sub(a,b){
+    return b-a
+}
+module.exports ={
+    sub
+}
